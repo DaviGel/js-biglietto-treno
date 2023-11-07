e0353aa1259915895fd99f8d7863633f61d233e1
DaviGel <geloso.davide@gmail.com>
2023-11-07 10:49:56 +0100

    fix: aggiunti controlli if e snellito il codice

--- js/main.js
+++ js/main.js
@@ -1,25 +1,29 @@
 'use strict';
 
-const kilometriTotali = parseInt(
-  prompt('Inserisci il numero di km da percorrere:')
-);
+const scontoMinorenni = 0.8;
+const scontoAnziani = 0.6;
 
-const etaPasseggero = parseInt(prompt("Inserisci l'età del passeggero:"));
+const kilometri = Number(prompt('Inserisci il numero di km da percorrere:'));
 
-let prezzoBiglietto = kilometriTotali * 0.21;
+const etaPasseggero = Number(prompt("Inserisci l'età del passeggero:"));
 
-/* N.B. dato che il metodo toFixed() restituisce una stringa,
-potrei convertire prezzoBiglietto.toFixed(2) in un numero,
-qualora fosse necessario, tramite la funzione parseInt()
-*/
-if (etaPasseggero > 0 && etaPasseggero < 18) {
-  prezzoBiglietto *= 0.8;
-  console.log('Il prezzo del biglietto è:', prezzoBiglietto.toFixed(2), '€');
-} else if (etaPasseggero > 65 && etaPasseggero <= 130) {
-  prezzoBiglietto *= 0.6;
-  console.log('Il prezzo del biglietto è:', prezzoBiglietto.toFixed(2), '€');
-} else if (etaPasseggero >= 18 && etaPasseggero <= 65) {
+if (
+  !isNaN(kilometri) &&
+  !isNaN(etaPasseggero) &&
+  kilometri > 0 &&
+  etaPasseggero > 0 &&
+  etaPasseggero <= 130
+) {
+  let prezzoBiglietto = kilometri * 0.21;
+
+  if (etaPasseggero < 18) {
+    prezzoBiglietto *= scontoMinorenni;
+  } else if (etaPasseggero > 65) {
+    prezzoBiglietto *= scontoAnziani;
+  }
+
+  /* N.B. dato che il metodo toFixed() restituisce una stringa, potrei convertire prezzoBiglietto.toFixed(2) in un numero, qualora fosse necessario, tramite la funzione Number() */
   console.log('Il prezzo del biglietto è:', prezzoBiglietto.toFixed(2), '€');
 } else {
-  console.log("Inserire un'età valida!");
+  console.log("Inserire i km validi e un'età valida in formato numerico!");
 }
